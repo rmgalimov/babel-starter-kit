b2ca022ea27cc34973aeb288ddbc5b770a946479
rmgalimov <rgalimov@es-mdcore.net>
2016-11-09 15:38:53 +0300

    Task2B
    Task2a & Task2b

--- src/index.js
+++ src/index.js
@@ -14,6 +14,26 @@ app.get('/task2A', (req, res) => {
   res.send(sum.toString());
 });
 
+app.get('/task2B', (req, res) => {
+  const fio = req.query.fullname.match(/[a-zA-Zа-яА-ЯёЁ\u00c0-\u00ff]+/gi) || '';
+  switch (fio.length) {
+    case 0:
+      res.send('Invalid fullname');
+      break;
+    case 1:
+      res.send(fio[0]);
+      break;
+    case 2:
+      res.send(fio[1] + ' ' + fio[0][0] + '.');
+      break;
+    case 3:
+      res.send(fio[2] + ' ' + fio[0][0] + '.' + ' ' + fio[1][0] + '.');
+      break;
+    default:
+      res.send('Invalid fullname');
+  }
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
