8d9a876c10bbd7274efa0e134281bd01e37f9dbc
rmgalimov <rgalimov@es-mdcore.net>
2016-11-11 16:31:41 +0300

    task2C
    Add Task2C

--- src/index.js
+++ src/index.js
@@ -18,21 +18,44 @@ app.get('/task2B', (req, res) => {
   if (req.query.fullname.match(/[0-9\\\/.,_]/)) {
     res.send('Invalid fullname');
   } else {
-    const fio = req.query.fullname.match(/[a-zA-Zа-яА-ЯёЁ\u00c0-\u00ff]+/gi) || '';
+    const fio = req.query.fullname.match(/[a-zA-Zа-яА-ЯёЁ\u00c0-\u00ff]+/gi) ||
+     '';
     switch (fio.length) {
       case 1:
         res.send(fio[0]);
         break;
       case 2:
-        res.send(`${fio[1][0].toUpperCase()}${fio[1].slice(1).toLowerCase()} ${fio[0][0].toUpperCase()}.`);
+        res.send(`${fio[1][0].toUpperCase()}${fio[1].slice(1).toLowerCase()} ${
+          fio[0][0].toUpperCase()}.`);
         break;
       case 3:
-        res.send(`${fio[2][0].toUpperCase()}${fio[2].slice(1).toLowerCase()} ${fio[0][0].toUpperCase()}. ${fio[1][0].toUpperCase()}.`);
+        res.send(`${fio[2][0].toUpperCase()}${fio[2].slice(1).toLowerCase()} ${
+          fio[0][0].toUpperCase()}. ${fio[1][0].toUpperCase()}.`);
         break;
       default:
         res.send('Invalid fullname');
         break;
     }
+  };
+});
+
+app.get('/task2C', (req, res) => {
+  const nickname1 = req.query.username.match(/[a-zA-Zа-яА-ЯёЁ\u00c0-\u00ff]+/gi);
+  if (nickname1.length == 1) {
+    console.log('0.' + nickname1);
+    console.log(nickname1.length);
+    res.send(`@${nickname1}`);
+  } else {
+    const nickname = req.query.username.match(/(\/)[a-zA-Zа-яА-ЯёЁ\u00c0-\u00ff]+/gi);
+    if (nickname.length == 0) {
+      console.log('1.' + nickname);
+      console.log(nickname.length);
+      res.send(`@${nickname}`);
+    } else {
+      console.log('2.' + nickname);
+      console.log(nickname.length);
+      res.send(`${nickname[(nickname.length - 1)].replace('/', '@')}`);
+    };
   };
 });
 
